4c3a2b8902dce4cc40189fe80a4c02034e82a61d
RoyLeviGit <tolevi@gmail.com>
2021-05-28 19:01:49 +0300

    Sections a, b, c supposedly complete :)

--- PeriodicNoise.py
+++ PeriodicNoise.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import matplotlib.image
 import matplotlib.pyplot as plt
@@ -11,8 +13,8 @@ def get_image(file_name):
     return image
 
 
-def get_noisy_image(image, noise_a):
-    noisy_image = image.copy()
+def get_noisy_image(original_image, noise_a):
+    noisy_image = original_image.copy()
     for k in range(int(2560 / 80)):
         for i in range(5):
             noisy_image_column = noisy_image[:, (k * 80) + (i * 16)]
@@ -48,32 +50,76 @@ def dft(dft_size):
     return dft_matrix / np.sqrt(dft_size)
 
 
-def plot_interference(noise_a, monster_dft):
+def get_notch_filter(interference_in_dft_domain):
+    filter_indices = np.array([x for x in range(2560)
+                               if abs(interference_in_dft_domain[x]) > 1e-8])
+    notch_filter = np.ones(2560)
+    notch_filter[filter_indices] = 0
+    notch_filter[0] = 1
+    return notch_filter
+
+
+def plot_interference_and_get_filter(noise_a, monster_dft):
     interference = np.zeros(2560)
     indices = np.array([x for x in range(2560) if x % 80 == 0])
     for i in range(5):
         interference[indices] = noise_a[i]
         indices += 16
     interference_in_dft_domain = (monster_dft @ interference).real
+
     plt.plot(interference_in_dft_domain)
     plt.suptitle("Interference in DFT domain")
     plt.show()
 
+    return get_notch_filter(interference_in_dft_domain)
+
+
+def get_restored_image(noisy_image, monster_dft, monster_dft_conjugate, notch_filter):
+    restored_image = noisy_image.copy()
+    for row in range(2560):
+        row_in_dft_domain = (monster_dft @ noisy_image[row, :])
+        row_in_dft_domain = row_in_dft_domain * notch_filter
+        restored_row = (monster_dft_conjugate @ row_in_dft_domain).real
+
+        # clip values out of grayscale
+        restored_row[restored_row > 255] = 255
+        restored_row[restored_row < 0] = 0
+        restored_image[row, :] = restored_row
+    return restored_image
+
+
+def mse(original_image, other_image):
+    mse_sum = 0
+    for row in range(2560):
+        for column in range(2560):
+            mse_sum += (original_image[row][column] - other_image[row][column]) ** 2
+    return mse_sum / (2560 ** 2)
+
+
+def compare_mse(original_image, noisy_image, restored_image):
+    print(f"Noisy image MSE: {mse(original_image, noisy_image)}")
+    print(f"Restored image MSE: {mse(restored_image, noisy_image)}")
+
 
 def periodic_noise():
     # Section a:
-    image = get_image("road.jpeg")
-    plot_image(image, "Original Image")
+    original_image = get_image("road.jpeg")
+    plot_image(original_image, "Original Image")
     noise_a = [int(x * 255 / 5) for x in range(-2, 4) if not x == 0]
     print(f"Noise to be added to columns of image: {noise_a}")
-    noisy_image = get_noisy_image(image, noise_a)
+    noisy_image = get_noisy_image(original_image, noise_a)
     plot_image(noisy_image, "Noisy Image")
 
     # Section b:
     monster_dft = dft(2560)
-    plot_interference(noise_a, monster_dft)
+    monster_dft_conjugate = monster_dft.conjugate()
+    notch_filter = plot_interference_and_get_filter(noise_a, monster_dft)
 
     # Section c:
+    restored_image = get_restored_image(noisy_image, monster_dft, monster_dft_conjugate, notch_filter)
+    plot_image(restored_image, "Restored Image")
+
+    compare_mse(original_image, noisy_image, restored_image)
 
 
 periodic_noise()
